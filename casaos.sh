8aeba7864a79127c6b1a4d30089a7449c7dc5563
Dvalin21 <dvalin21@gmail.com>
2023-05-31 15:55:17 -0500

    Update casaos.sh
    Added Coral EdgeTpu PCIE detection and install.

--- casaos.sh
+++ casaos.sh
@@ -20,10 +20,23 @@ if lspci | grep -i amd; then
   echo "AMD GPU detected"
 sudo apt-get update
 wget https://repo.radeon.com/amdgpu-install/22.40.5/ubuntu/jammy/amdgpu-install_5.4.50405-1_all.deb
-sudo apt-get install ./amdgpu-install_5.4.50404-1_all.deb -y #Required to hit "Enter" after install and then the rest will continue.
+sudo chmod +x amdgpu-install_5.4.50405-1_all.deb
+sudo apt-get install ./amdgpu-install_5.4.50405-1_all.deb -y #Required to hit "Enter" after install and then the rest will continue.
 sudo amdgpu-install --usecase=rocm -y --accept-eula --no-32 && sudo apt install vainfo radeontop && sudo apt install docker-compose && sudo apt install libgl1-mesa-glx libgl1-mesa-dri xserver-xorg-video-amdgpu -y && sudo usermod -a -G video $LOGNAME && sudo usermod -a -G render $LOGNAME
 else
   echo "AMD GPU not detected"
+fi
+
+if lspci -nn | grep 089a; then
+  echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install gasket-dkms libedgetpu1-std
+sudo sh -c "echo 'SUBSYSTEM==\"apex\", MODE=\"0660\", GROUP=\"apex\"' >> /etc/udev/rules.d/65-apex.rules"
+sudo groupadd apex
+sudo adduser $USER apex
+else
+  echo "Coral EdgeTpu PCIE not detected"
 fi
 
 #Installing Git if missing
@@ -40,3 +53,6 @@ sudo apt-get update
 sudo apt-get install aptitude
 sudo aptitude safe-upgrade -y
 sudo apt-get autoremove
+
+# System Restart
+sudo reboot
